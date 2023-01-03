7d0f79dc26000fc2192c7f75e377c254f423a7a6
cameron-schlueter <camschlueter@gmail.com>
2023-01-02 22:03:35 -0500

    added the start of saving inputs to DOM. Added const for all times.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
-const saveBtn9am = document.getElementsByClassName('btn saveBtn col-2 col-md-1');
-const input9am = document.getElementById('fas fa-save');
+const saveBtn9am = document.getElementsByClassName('row time-block past');
+const input9am = document.getElementById('hour-9');
+
+const saveBtn10am = document.getElementsByClassName('row time-block present');
+const input10am = document.getElementById('hour-10');
+
+const saveBtn11am = document.getElementsByClassName('row time-block future');
+const input11am = document.getElementById('hour-11');
 //make sure to do the other inputs too
 
 
@@ -10,7 +16,7 @@ const input9am = document.getElementById('fas fa-save');
 // looking to save the input data in DOM. definitely need to appendChild, but where to after that?
 
 
-  $(document).ready(function() {
+$(document).ready(function() {
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,17 +27,17 @@ const input9am = document.getElementById('fas fa-save');
  
 
   //event listener for the save button and local storage
-  saveBtn9am.addEventListener('click',function() {
+$saveBtn9am.addEventListener('click',function() {
     const input9am = inputField.value;
     localStorage.setItem('fas fa-save', input9am);
   });
 
-  saveBtn10am.addEventListener('click',function() {
+$saveBtn10am.addEventListener('click',function() {
     const input10am = inputField.value;
     localStorage.setItem('fas fa-save', input10am);
   });
 
-saveBtn11am.addEventListener('click',function() {
+$saveBtn11am.addEventListener('click',function() {
     const input11am = inputField.value;
     localStorage.setItem('fas fa-save', input11am);
   });
@@ -45,12 +51,56 @@ saveBtn11am.addEventListener('click',function() {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
+  // past DOM inputs
+  $(document).ready(function() {
+    // Save user input to local storage
+    $('#row time-block past').on('input', function() {
+      var input = $(this).val();
+      localStorage.setItem('col-8 col-md-10 description', input);
+    });
+  
+    // Set the value of the textarea element from local storage
+    var savedInput = localStorage.getItem('col-8 col-md-10 description');
+    if (savedInput) {
+      $('#row time-block past').val(savedInput);
+    }
+  });
+  
+  // present DOM inputs
+  $(document).ready(function() {
+    // Save user input to local storage
+    $('#row time-block present').on('input', function() {
+      var input = $(this).val();
+      localStorage.setItem('col-8 col-md-10 description', input);
+    });
+  
+    // Set the value of the textarea element from local storage
+    var savedInput = localStorage.getItem('col-8 col-md-10 description');
+    if (savedInput) {
+      $('#row time-block present').val(savedInput);
+    }
+  });
+  
+  // future DOM inputs
+  $(document).ready(function() {
+    // Save user input to local storage
+    $('#row time-block future').on('input', function() {
+      var input = $(this).val();
+      localStorage.setItem('col-8 col-md-10 description', input);
+    });
+  
+    // Set the value of the textarea element from local storage
+    var savedInput = localStorage.getItem('col-8 col-md-10 description');
+    if (savedInput) {
+      $('#row time-block future').val(savedInput);
+    }
+  });
+  
   // TODO: Add code to display the current date in the header of the page.
 
-
   //why is this not working now?
-  function updateClock() {
+function updateClock() {
     const now = dayjs();
     const time = now.format('dddd, MMMM D, YYYY, hh:mm:ss');
     document.getElementById('currentDay').innerHTML = time;
